b5f8b02148c011813977aacccd2ac5e6c652aebb
GIORGI SADIRINI <gogliks98@gmail.com>
2022-11-11 16:26:15 +0400

    modified  navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css'
-import Home from './components/Home/Home';
-import NavBar from './components/NavBar/NavBar';
-import Projects from './components/Projects/Projects';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-     <NavBar/>
-     <div>
-       
-      </div>
-      <Footer/>
+      <NavBar />
+      <Footer />
     </div>
   );
 }
--- src/components/CreateGroup/CreateGroup.jsx
+++ src/components/CreateGroup/CreateGroup.jsx
@@ -5,12 +5,10 @@ const CreateGroup = () => {
     <div className="form">
       <h1>CreateGroup</h1>
       <form>
-        <input placeholder="enter group name"/>
-        <input placeholder="add students"/>
-        <input placeholder="add tasks"/>
+        <input placeholder="enter group name" />
+        <input placeholder="add students" />
+        <input placeholder="add tasks" />
         <button>Submit</button>
-
-
       </form>
     </div>
   );
